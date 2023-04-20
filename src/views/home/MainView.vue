20213538ce16fdbc93be979b649b2534cd5192f6
DESKTOP-HC4CT58\initbyran <kmr4766@naver.com>
2023-04-20 14:55:31 +0900

    init

---
+++ src/views/home/MainView.vue
@@ -0,0 +1,16 @@
+<template>
+    <basic-layout>
+
+    </basic-layout>
+</template>
+
+<script>
+import BasicLayout from '@/components/basic/BasicLayout.vue';
+
+export default{
+    name : 'MainView',
+    components : {
+        BasicLayout
+    }
+};
+</script>
