e5c4d7657fc7d6d3052f578f77c355c89c9e2c59
DESKTOP-HC4CT58\initbyran <kmr4766@naver.com>
2023-04-20 15:33:42 +0900

    member header 초안

---
+++ src/modules/layout/components/HeaderComp.vue
@@ -0,0 +1,33 @@
+<template>
+
+  <v-app-bar
+    color="white"
+  >
+    
+    <v-app-bar-title></v-app-bar-title>
+    
+    <v-spacer></v-spacer>
+    
+    <v-btn icon>
+      <v-icon>mdi-heart</v-icon>
+    </v-btn>
+    
+    <v-btn icon>
+      <v-icon>mdi-bell</v-icon>
+    </v-btn>
+    
+    <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    
+  </v-app-bar>
+
+</template>
+
+<script>
+export default {
+    name:'HeaderComp'
+}
+</script>
+
+<style>
+
+</style>
