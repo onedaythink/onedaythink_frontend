7a490ab46803f471955e87db1211e949ac3758d8
sjh <zwielicht2@gmail.com>
2023-04-23 21:47:46 +0900

    test

--- src/modules/layout/MainLayout.vue
+++ src/modules/layout/MainLayout.vue
@@ -1,6 +1,6 @@
 <template>
   <div>초기화면</div>
-  <div>로그인</div>
+  <router-link>로그인</div>
   <router-link to="/home">user layout 이동</router-link><br>
   <router-link to="/admin">admin layout 이동</router-link>
 </template>
