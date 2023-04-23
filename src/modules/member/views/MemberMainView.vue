d9ee9bad2893ba15605268d4d740b2c013e2fec9
DESKTOP-HC4CT58\initbyran <kmr4766@naver.com>
2023-04-24 00:40:00 +0900

    로그인 후 메인 화면 수정 : 투명 스크롤바, navigation-drawer(메뉴바) 추가, 이미지 추가, 질문 펼침/닫힘  버튼 생성

--- src/modules/member/views/MemberMainView.vue
+++ src/modules/member/views/MemberMainView.vue
@@ -19,10 +19,17 @@ export default {
 </script>
 
 <style>
+/* 스크롤바 관련 설정 */
   .subject-card-wrapper {
-  margin-top: 100x;
+  max-height: 500px;
+  overflow-y: auto;
+  margin-top: 60px;
   margin-bottom: 60px;
 }
 
+/* 스크롤바 숨기기 */
+.subject-card-wrapper::-webkit-scrollbar {
+  display: none;
+}
 
 </style>
