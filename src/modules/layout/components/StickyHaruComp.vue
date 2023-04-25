cb39e10c0928e6c68271734f6868687e44cd0b9d
DESKTOP-HC4CT58\initbyran <kmr4766@naver.com>
2023-04-25 15:37:05 +0900

    2023-04-25 vuetify

---
+++ src/modules/layout/components/StickyHaruComp.vue
@@ -0,0 +1,20 @@
+<template>
+   <div class="floating-button">
+    <v-row justify="end">
+      <v-col cols="auto">
+        <v-btn color="pink-lighten-5" icon="mdi-face" size="large"></v-btn>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+  .floating-button {
+    position: fixed;
+    margin-bottom: 15px;
+  }
+</style>
